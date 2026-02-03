fa6c0708efba2438e51e5b21e3613900970298f6
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-02-03 11:39:38 +0100

    03021139

---
+++ Les01/herhaling_operatoren/herhaling_operatoren/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herhaling_operatoren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            x++;
+            Console.WriteLine(x); // 6
+            Console.WriteLine(x++); // 6
+            Console.WriteLine(x); // 7
+            Console.WriteLine(++x); // 8
+        }
+    }
+}
