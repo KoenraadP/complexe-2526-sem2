6e896ea56bd4ac2bec80636934e3119dd97b1ce7
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-02-24 10:50:05 +0100

    24021049

--- Les03/herhaling_methodes/herhaling_methodes/Program.cs
+++ Les03/herhaling_methodes/herhaling_methodes/Program.cs
@@ -18,11 +18,14 @@ namespace herhaling_methodes
             Console.WriteLine(sum);
             Console.WriteLine(Add(nr1, nr2));
 
+            Console.WriteLine(Divide(nr1, nr2));
+
             double nr3 = 10.5;
             double nr4 = 11.6;
             Console.WriteLine(Add(nr3, nr4));
-
             Console.WriteLine(Add(nr2));
+
+            CountdownToZero(10);
         }
 
         // methode om twee int cijfers op te tellen
@@ -41,6 +44,34 @@ namespace herhaling_methodes
         {
             return x + y;
             //return Convert.ToInt32(m + n);
+        }
+
+        // voorbeeld methode met 'recursion'
+        // methode roept zichzelf aan binnen de methode body
+        private static void CountdownToZero(int nr)
+        {
+            if (nr == 0)
+            {
+                Console.WriteLine("Done!");
+                // return --> stopt de methode
+                return;
+            }
+
+            Console.WriteLine(nr);
+            CountdownToZero(nr - 1);
+        }
+
+        public static double Divide(int number1, int number2)
+        {
+            // maak hier je berekening, je zal de "int" waarden met behulp van Convert.ToDouble moeten omzetten naar kommagetallen => bvb. double x = Convert.ToDouble(100);
+            // Controleer in deze methode ook of de 2 waarde groter is dan 0...want delen door 0 kan niet!
+            // Als het tweede cijfer een 0 is, return dan gewoon -1
+
+            if (number2 == 0) return -1;
+
+            double result = 0;
+            result = Convert.ToDouble(number1) / Convert.ToDouble(number2);
+            return result;
         }
     }
 }
