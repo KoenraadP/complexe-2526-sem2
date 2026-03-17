73c3b725d27ba29f639c8b6e60301ed97f53a6fa
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-03-17 09:37:48 +0100

    1703937

--- Les06/EnumsDemo/EnumsDemo.Cons/Program.cs
+++ Les06/EnumsDemo/EnumsDemo.Cons/Program.cs
@@ -12,6 +12,12 @@ namespace EnumsDemo.Cons
 
             // enum dag uittesten
             Console.WriteLine(Convert.ToInt32(Dag.Zondag));
+
+            // omgekeerd --> cijfer omzetten naar season enum type
+            Console.WriteLine((Season)2);
+
+            Console.WriteLine(Country.BE);
+            Console.WriteLine((Country)22);
         }
     }
 }
