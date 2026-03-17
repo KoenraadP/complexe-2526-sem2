de55d29655ddc786f7f365067eba1b4d172a0d4f
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-03-17 09:32:02 +0100

    1703931

---
+++ Les06/EnumsDemo/EnumsDemo.Cons/Program.cs
@@ -0,0 +1,17 @@
+﻿using EnumsDemo.Entities;
+using System;
+
+namespace EnumsDemo.Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // enum season uittesten
+            Console.WriteLine(Season.Winter);
+
+            // enum dag uittesten
+            Console.WriteLine(Convert.ToInt32(Dag.Zondag));
+        }
+    }
+}
---
+++ Les06/EnumsDemo/EnumsDemo.Entities/MyEnums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsDemo.Entities
+{
+    public enum Season
+    {
+        Spring, 
+        Summer,
+        Autumn,
+        Winter
+    }
+
+    // bij enum dag veranderen we de cijfer waarde
+    // zodat deze niet meer bij 0 start
+    public enum Dag
+    {
+        Maandag = 1,
+        Dinsdag,
+        Woensdag,
+        Donderdag,
+        Vrijdag,
+        Zaterdag,
+        Zondag
+    }
+}
