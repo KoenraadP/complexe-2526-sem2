764acafbfc514da541cea04763f80234f956718f
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-03-24 09:24:03 +0100

    2403923

---
+++ Les07/SaveText/SaveText.Bll/TextBll.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace SaveText.Bll
+{
+    internal class TextBll
+    {
+        // methode die een string kan opslaan
+        // in een tekstbestand op de computer
+        // binnen een specifieke map
+        // map = directory (of folder) in het Engels
+        // voorbeeld directory: C:\verhalen\
+        // voorbeeld title: doornroosje
+        // voorbeeld text: het volledige verhaal
+        public void SaveText(string directory,
+                            string title, string text)
+        {
+            // opbouwen pad om op te slaan, inclusief bestandsextensie
+            string fullPath = Path.Combine(directory, title + ".txt");
+
+            // File is een ingebouwde class
+            // uit de System.IO library
+            // WriteAllText vereist een 'pad' --> voorbeeld c:\verhalen\doornroosje.txt
+            // en contents --> de volledige tekst die in de file moet staan
+            File.WriteAllText(fullPath, text);
+        }
+    }
+}
