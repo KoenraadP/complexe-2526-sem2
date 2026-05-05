d5f26907e0bc59f31e0cb156ae8a5a8728c7b21c
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-05-05 10:45:37 +0200

    05051045

---
+++ Les11/Les11_Events/MovePicture/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace MovePicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.pbxImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(86, 12);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 0;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.BtnUp_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(158, 41);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.BtnRight_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(86, 70);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.BtnDown_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(12, 41);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 3;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.BtnLeft_Click);
+            // 
+            // pbxImage
+            // 
+            this.pbxImage.Image = global::MovePicture.Properties.Resources.idris;
+            this.pbxImage.Location = new System.Drawing.Point(275, 131);
+            this.pbxImage.Name = "pbxImage";
+            this.pbxImage.Size = new System.Drawing.Size(66, 93);
+            this.pbxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxImage.TabIndex = 4;
+            this.pbxImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 354);
+            this.Controls.Add(this.pbxImage);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnUp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.PictureBox pbxImage;
+    }
+}
+
---
+++ Les11/Les11_Events/MovePicture/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovePicture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDown_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(pbxImage.Top.ToString());
+            // afstand tot aan de bovenkant (top) van mijn formulier vergroten
+            pbxImage.Top += 10;
+        }
+
+        private void BtnUp_Click(object sender, EventArgs e)
+        {
+            pbxImage.Top -= 10;
+            // pbxImage.Bottom += 10; // dit werkt niet
+        }
+
+        private void BtnRight_Click(object sender, EventArgs e)
+        {
+            pbxImage.Left += 10;
+        }
+
+        private void BtnLeft_Click(object sender, EventArgs e)
+        {
+            pbxImage.Left -= 10;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // knoppen uitschakelen om key event te doen werken
+            // als je deze toch weer wil gebruiken, zet dan deze code gewoon in comment
+            btnDown.Enabled = false;
+            btnUp.Enabled = false;
+            btnRight.Enabled = false;
+            btnLeft.Enabled = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // als ik op het pijltje omlaag druk...
+            if (e.KeyCode == Keys.Down) pbxImage.Top += 10;
+            if (e.KeyCode == Keys.Up) pbxImage.Top -= 10;
+            if (e.KeyCode == Keys.Right) pbxImage.Left += 10;
+            if (e.KeyCode == Keys.Left) pbxImage.Left -= 10;
+
+            // alternatief met switch voorbeeld
+            //switch (e.KeyCode)
+            //{
+            //    case Keys.Down:
+            //        pbxImage.Top += 10;
+            //        break;
+            //}
+        }
+    }
+}
