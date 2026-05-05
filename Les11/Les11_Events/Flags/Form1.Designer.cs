d8f4aaccfce05293c11dd3d81e59617402bd39db
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-05-05 10:07:15 +0200

    05051007

---
+++ Les11/Les11_Events/Flags/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Flags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxLanguage = new System.Windows.Forms.ComboBox();
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.pbxFlag = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFlag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxLanguage
+            // 
+            this.cbxLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxLanguage.FormattingEnabled = true;
+            this.cbxLanguage.Items.AddRange(new object[] {
+            "Nederlands",
+            "English",
+            "Français",
+            "Deutsch"});
+            this.cbxLanguage.Location = new System.Drawing.Point(13, 13);
+            this.cbxLanguage.Name = "cbxLanguage";
+            this.cbxLanguage.Size = new System.Drawing.Size(197, 21);
+            this.cbxLanguage.TabIndex = 0;
+            this.cbxLanguage.SelectedIndexChanged += new System.EventHandler(this.CbxLanguage_SelectedIndexChanged);
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.Location = new System.Drawing.Point(13, 41);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(71, 13);
+            this.lblGreeting.TabIndex = 1;
+            this.lblGreeting.Text = "Hallo Wereld!";
+            // 
+            // pbxFlag
+            // 
+            this.pbxFlag.Location = new System.Drawing.Point(13, 58);
+            this.pbxFlag.Name = "pbxFlag";
+            this.pbxFlag.Size = new System.Drawing.Size(197, 139);
+            this.pbxFlag.TabIndex = 2;
+            this.pbxFlag.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 214);
+            this.Controls.Add(this.pbxFlag);
+            this.Controls.Add(this.lblGreeting);
+            this.Controls.Add(this.cbxLanguage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFlag)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxLanguage;
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.PictureBox pbxFlag;
+    }
+}
+
---
+++ Les11/Les11_Events/Flags/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flags
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // eerste element uit combobox al actief plaatsen
+            cbxLanguage.SelectedIndex = 0;
+        }
+
+        private void CbxLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxLanguage.SelectedIndex == 0)
+            {
+                lblGreeting.Text = "Goedemorgen";
+            }
+
+            //if (cbxLanguage.SelectedItem.ToString() == "English")
+            if (cbxLanguage.SelectedIndex == 1)
+            {
+                lblGreeting.Text = "Good morning!";
+            }
+
+            if (cbxLanguage.SelectedIndex == 2)
+            {
+                lblGreeting.Text = "Bonjour!";
+            }
+
+            if (cbxLanguage.SelectedIndex == 3)
+            {
+                lblGreeting.Text = "Guten Morgen!";
+            }
+
+            //switch (cbxLanguage.SelectedIndex)
+            //{
+            //    case 0:
+            //        lblGreeting.Text = "Goedemorgen!";
+            //        break;
+            //    case 1:
+            //        lblGreeting.Text = "Good morning!";
+            //        break;
+            //}
+        }
+    }
+}
